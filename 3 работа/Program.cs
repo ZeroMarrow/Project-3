b449a8c841b996f55cece8fa512392f8d6fb4b94
ZeroMarrow <71882927+ZeroMarrow@users.noreply.github.com>
2020-10-02 16:36:44 +0200

    Add files via upload

---
+++ 3 работа/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_работа
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            bool r = (y < Math.Abs(x)) && (x > -1) && (x < 1);
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
